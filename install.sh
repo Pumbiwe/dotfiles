6174e3b7457cb5cdc5b12247974224bd9f34aebd
Pumbiwe <111291491+Pumbiwe@users.noreply.github.com>
2026-02-28 10:56:44 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ cp -r wallpaper ~/
 cp -r config/* ~/.config
 
 # zshrc
-sudo echo "$(<config/.zshrc )" > ~/.zshrc
+sudo echo "$(<.zshrc )" > ~/.zshrc
 
 # Firefox theme
 curl https://raw.githubusercontent.com/gh0stzk/dotfiles/master/misc/firefox/user.js -o ~/.mozilla/firefox/user.js
