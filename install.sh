c7331e1b085b1f7f90d9095f7d162bd6b9fc3ed8
Pumbiwe <111291491+Pumbiwe@users.noreply.github.com>
2026-02-28 12:22:29 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,19 +25,115 @@ git clone https://github.com/Pumbiwe/dotfiles
 cd dotfiles
 echo "Success."
 
-sudo pacman -Syu sddm awesome git nautilus alacritty vim neovim code polkit-gnome rofi zsh fastfetch lxappearance dpkg qt6-5compat qt5 loupe scrot cmake firefox debugedit make fakeroot gcc i3-wm i3status feh polybar github-cli --noconfirm
+packages=(sddm 
+  awesome 
+  git 
+  nautilus 
+  alacritty 
+  vim 
+  neovim
+  code
+  polkit-gnome
+  rofi
+  zsh
+  fastfetch
+  lxappearance
+  dpkg
+  qt6-5compat
+  qt5
+  loupe
+  scrot
+  cmake
+  firefox
+  debugedit
+  make
+  fakeroot
+  gcc
+  i3-wm
+  i3status
+  feh
+  polybar
+  github-cli)
 
+echo "List of packages: "
+n=1
+for var in "${packages[@]}"; do
+  echo "$n) $var"
+  n=$((n + 1))
+done
+echo -n "Do you want to install these packages? [y/n]: "
+read answer
+if [[ 'n' == $answer || 'N' == $answer ]]; then
+  echo "Ok, bye!"
+  exit 0
+fi
+sudo pacman -Syu
+for var in "${packages[@]}"; do
+  sudo pacman -S $var --noconfirm
+  if [[ $? == 1 ]]; then
+    echo -e "\033[0;31m[ERROR] Package \033[0;34m$var\033[0;31m did not installed.\033[0;37m"
+    read -p "Press [Enter] to continue..."
+  fi
+done
+
+
+echo -n "Do you want to install zsh? [y/n]: "
+read answer
+if [[ 'n' == $answer || 'N' == $answer ]]; then
+  echo "Ok."
+else
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo echo "$(<.zshrc)" >~/.zshrc
+fi
+
+
+echo -n "Do you want to install yay? [y/n]: "
+read answer
+if [[ 'n' == $answer || 'N' == $answer ]]; then
+  echo "Ok."
+  exit 0
+else
 # Install yay
 git clone https://aur.archlinux.org/yay.git ~/tmp/yay
 (cd ~/tmp/yay && makepkg -si --noconfirm)
+fi
 
-# Installing paru pkgs
-yay -S ttf-iosevka-nerd catppuccin-cursors-mocha orchis-theme postman-bin sddm-theme-noctalia-git --noconfirm
+packages=(ttf-iosevka-nerd 
+  catppuccin-cursors-mocha
+  orchis-theme
+  postman-bin
+  sddm-theme-noctalia-git)
 
+echo "List of packages: "
+n=1
+for var in "${packages[@]}"; do
+  echo "$n) $var"
+  n=$((n + 1))
+done
+echo -n "Do you want to install these packages? [y/n]: "
+read answer
+if [[ 'n' == $answer || 'N' == $answer ]]; then
+  echo "Ok, bye!"
+  exit 0
+fi
+for var in "${packages[@]}"; do
+  yay -S $var --noconfirm
+  if [[ $? == 1 ]]; then
+    echo -e "\033[0;31m[ERROR] Package \033[0;34m$var\033[0;31m did not installed.\033[0;37m"
+    read -p "Press [Enter] to continue..."
+  fi
+done
+
+
+echo -n "Do you want to install noctalia sddm theme? [y/n]: "
+read answer
+if [[ 'n' == $answer || 'N' == $answer ]]; then
+  echo "Ok."
+else
 # sddm theme
 echo "[Theme]
 Current=noctalia" | sudo tee /etc/sddm.conf
+fi
 
 # Set wallpaper
 cp -r wallpaper ~/
@@ -45,9 +141,11 @@ cp -r wallpaper ~/
 # Themes
 cp -r config/* ~/.config
 
-# zshrc
-sudo echo "$(<.zshrc)" >~/.zshrc
-
+echo -n "Do you want to install firefox theme? [y/n]: "
+read answer
+if [[ 'n' == $answer || 'N' == $answer ]]; then
+  echo "Ok."
+else
 # Firefox theme
 curl https://raw.githubusercontent.com/gh0stzk/dotfiles/master/misc/firefox/user.js -o ~/.mozilla/firefox/user.js
 mkdir -p ~/.mozilla/firefox/chrome
@@ -56,17 +154,33 @@ curl https://raw.githubusercontent.com/gh0stzk/dotfiles/master/misc/firefox/chro
 curl https://raw.githubusercontent.com/gh0stzk/dotfiles/master/misc/firefox/chrome/right-arrow.svg -o ~/.mozilla/firefox/chrome/right-arrow.svg
 curl https://raw.githubusercontent.com/gh0stzk/dotfiles/master/misc/firefox/chrome/userChrome.css -o ~/.mozilla/firefox/chrome/userChrome.css
 curl https://raw.githubusercontent.com/gh0stzk/dotfiles/master/misc/firefox/chrome/userContent.css -o ~/.mozilla/firefox/chrome/userContent.css
+fi
 
-# Spacemacs
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-
+echo -n "Do you want to install rofi theme? [y/n]: "
+read answer
+if [[ 'n' == $answer || 'N' == $answer ]]; then
+  echo "Ok."
+else
 # Rofi theme
 mkdir -p ~/.local/share/rofi/themes
 cp rofi/spotlight-dark.rasi ~/.local/share/rofi/themes/
+fi
 
+echo -n "Do you want to install LazyVim? [y/n]: "
+read answer
+if [[ 'n' == $answer || 'N' == $answer ]]; then
+  echo "Ok."
+else
 # LazyVim
 git clone https://github.com/LazyVim/starter ~/.config/nvim
+fi
 
-echo "Successfully installed dotfiles! Have a nice day!"
+
+echo -n "Do you want to enable and restart sddm? [y/n]: "
+read answer
+if [[ 'n' == $answer || 'N' == $answer ]]; then
+  echo "Ok."
+else
 systemctl enable sddm
 systemctl restart sddm
+fi
